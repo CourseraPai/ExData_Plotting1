3e58c884322a4cab1cb66d6c0a8450f386c6d2ff
billerica <mp_citrix@yahoo.com>
2015-12-21 16:22:37 -0500

    commit

--- Plot1.R
+++ Plot1.R
@@ -1,4 +1,4 @@
-tab <- read.table("household_power_consumption.txt",fill=TRUE, header = FALSE,sep=";",skip=(grep("1/2/2007", readLines("household_power_consumption.txt"))-1),nrows=2880)
+tab  <- read.table("household_power_consumption.txt",fill=TRUE, header = FALSE,sep=";",skip=(grep("1/2/2007", readLines("household_power_consumption.txt"))-1),nrows=2880)
 names(tab)<-c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 png(filename="plot1.png",width=480,height=480)
 par(oma=c(1,1,1,1))
